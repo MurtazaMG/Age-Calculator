19c2c1cddcc3e33bcc08450ff0c7f2f67b17fb54
MurtazaMG <46533299+MurtazaMG@users.noreply.github.com>
2019-01-13 18:46:58 -0500

    Add files via upload

---
+++ Age Calculator.py
@@ -0,0 +1,8 @@
+num1 = 0
+num2 = 0
+age = 0
+num1 = int(input("Enter your year of birth: "))
+num2 = int(input("Enter the current year: "))
+age = (num2-num1)
+print("Your age is approximately",age,"years old.")
+ 
